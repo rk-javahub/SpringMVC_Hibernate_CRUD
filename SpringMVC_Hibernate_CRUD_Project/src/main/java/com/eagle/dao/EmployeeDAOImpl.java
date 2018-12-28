9affb3610d56bcef9a26da618dd642baeec0460a
rohit <kumbharrohit13@gmail.com>
2018-12-28 17:33:04 +0530

    EmployeeDAOImpl file created

---
+++ SpringMVC_Hibernate_CRUD_Project/src/main/java/com/eagle/dao/EmployeeDAOImpl.java
@@ -0,0 +1,52 @@
+package com.eagle.dao;
+
+import java.util.List;
+
+import com.eagle.entity.Employee;
+
+/**
+ * @author Rohit
+ *
+ */
+public class EmployeeDAOImpl implements EmployeeDAO {
+	public EmployeeDAOImpl() {
+		super();
+	}
+
+	@Override
+	public long createEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteEmployee(long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee getEmployee(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmployees(String employeeName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
